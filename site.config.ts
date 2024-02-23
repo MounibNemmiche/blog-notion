0122ff76e310e5b98876c5ce50a91fe2a3fdc7a3
Mounib <87337340+MounibNemmiche@users.noreply.github.com>
2024-02-23 22:01:20 +0100

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -1,19 +1,19 @@
 import { siteConfig } from "./lib/site-config";
 
 export default siteConfig({
-  rootNotionPageId: "54755ccc1a914a59806e07e532da3970",
-  rootNotionSpaceId: "a216946e-b6f5-43f3-9663-c762a0ac71d9",
+  rootNotionPageId: "61b797ca7b2a4a83951f7264e76b9abd",
+  rootNotionSpaceId: null,
   name: `Kai's Dev Blog`,
   domain: "blog.kaichi.dev",
   author: "kaichi",
-  defaultPageIcon: "https://blog.kaichi.dev/page-icon.png",
-  defaultPageCover: "https://blog.kaichi.dev/page-cover.jpg",
+  defaultPageIcon: null,
+  defaultPageCover: null,
   defaultPageCoverPosition: 0.5,
   navigationStyle: "custom",
   navigationLinks: [
     {
       pageId: "689537b0ba494fc4a550e4ce2fc56bda",
-      title: "关于",
+      title: "About",
       url: "/about",
     },
   ],
